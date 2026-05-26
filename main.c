5558c8068bba763a42b5ee95df91495494d44cd3
AvijitBiswasAvi <aviavijitbiswas35@gmail.com>
2026-05-26 10:51:34 +0600

    2nd day

--- main.c
+++ main.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
-    
-    srand (time(0));
+int main()
+{
+
+    srand(time(0));
 
     int random_1 = rand() % 100 + 1;
     int random_2 = rand() % 200 + 1;
@@ -13,6 +14,7 @@ int main() {
     int no_of_guesses = 0;
     int guessed;
     int choice;
+    int score;
 
     printf("==== YOUR OPTIONS ====\n");
     printf("1. Easy (1 --> 100)\n");
@@ -23,8 +25,46 @@ int main() {
     printf("Enter your choice = ");
     scanf("%d", &choice);
 
-    
+    switch (choice)
+    {
+    case 1:
+        do
+        {
+            printf("Guess the number = ");
+            scanf("%d", &guessed);
 
+            if (guessed > random_1)
+            {
+                printf("Try lower number\n");
+            }
+            else if (guessed < random_1)
+            {
+                printf("Try higher number\n");
+            }
+            else
+            {
+                printf("--! Congratulations !--\n");
+            }
+            no_of_guesses++;
+
+            if (no_of_guesses == 10)
+            {
+                printf("--! GAME OVER !--");
+                break;
+            }
+
+        } while (guessed != random_1);
+
+        score = (10 - no_of_guesses + 1) * 100;
+
+        printf("Number of gusses = %d\n", no_of_guesses);
+        printf("You'r score = %d\n", score);
+
+        break;
+
+    default:
+        break;
+    }
 
     return 0;
 }
